1e31a8eba4233604915fe8849a54d6d6d00ae3b5
jannik <jjen527@g.educaand.es>
2025-04-07 13:48:57 +0200

    actualizacion del proyecto para que funciona la publicacion

--- modulos/publicaciones.py
+++ modulos/publicaciones.py
@@ -23,6 +23,22 @@ def mostrar_menu():
         elif opcion == "2":
             listar_publicaciones()
 
+        elif opcion == "3":
+            listar_publicacion_por_usuario()
+
+        elif opcion == "4":
+            editar_contenido()
+        
+        elif opcion == "5":
+            eliminar_publicacion()
+
+        elif opcion == "6":
+            print("Volviendo al menu principal")
+            break
+
+        else:
+            print("Opcion invalida. Intente de nuevo.")
+
 
 
 
@@ -58,7 +74,82 @@ def listar_publicaciones():
             return
         
         for publicacion in publicaciones:
-            print(f'{publicacion.id} {publicacion.usuario_id} {publicacion.contenido} {publicacion.fecha_publi}')
+            print(f'{publicacion.id} {publicacion.id_usuario} {publicacion.contenido} {publicacion.fecha_publi}')
 
     except Exception as e:
         logging.error(f'Error al listar las publicaciones: {e}')
+
+
+
+def listar_publicacion_por_usuario():
+    print("\n Publicacion por usuario")
+
+    try:
+        usuario = int(input("Introduce el ID del usuario: "))
+        publicacion = Publicacion.obtener_publi_por_usuario(usuario)
+        
+        for i in publicacion:
+            print(f'{i.id} {i.id_usuario} {i.contenido} {i.fecha_publi}')
+
+
+    
+    except Exception as e:
+        logging.error(f'Error al listar las publicaciones: {e}')
+
+
+def editar_contenido():
+    print("\n Edicion de contenido")
+
+    try:
+        id_publicacion = input("Introduce el ID de la publicacion: ")
+        publicacion = Publicacion.obtener_por_id(int(id_publicacion))
+        if not publicacion:
+            print(f'No existe una publicacion con ID {id_publicacion}')
+            return
+        
+        print(f'Contenido: {publicacion.contenido}')
+
+        content = input("Nuevo contenido: ")
+
+        if publicacion.editar_contenido(content):
+            print("Contenido actualizado correctamente.")
+
+        else:
+            print("No se podia actualizar el contenido")
+
+    except Exception as e:
+        logging.error(f'Error al editar el contenido: {e}')
+
+
+
+def eliminar_publicacion():
+    """Elimina una publicacion"""
+    print("\n ELIMINAR PUBLICACION")
+    id_publicacion = input("ID de la publicacion a eliminar: ")
+    if not id_publicacion.isdigit():
+        print("El ID debe ser un numero.")
+        return
+    
+    try:
+        publicacion = Publicacion.obtener_por_id(int(id_publicacion))
+        if not publicacion:
+            print(f"No existe una publicacion con ID {id_publicacion}.")
+            return
+        
+        print(f'ID: {publicacion.id} ID de Usuario {publicacion.id_usuario}')
+        print(f'Contenido: {publicacion.contenido}')
+        print(f'Fecha de publicacion: {publicacion.fecha_publi}')
+        print("\n ADVERTENCIA: Esto elimina la publicacion definitivamente")
+        confirmacion = input("¿Estas seguro de eliminar esta publicacion? (s/n): ").lower()
+
+        if confirmacion == "s":
+            if publicacion.eliminar_publicacion():
+                print("Publicacion eliminado correctamente")
+            else:
+                print("No se pudo eliminar la publicacion.")
+                
+        else:
+            print("Operacion cancelada.")
+
+    except Exception as e:
+        logging.error(f'Error al eliminar publicacion: {e}')
